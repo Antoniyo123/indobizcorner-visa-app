9006787e8e2e769247e7b44fc92555938eee0ab9
Riswanton Ramudin <riswantonr2015@gmail.com>
2025-07-25 15:10:54 +0700

    change localhost

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base URL backend
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_BASE_URL = process.env.REACT_APP_API_URL || 'indobizcorner.com';
 
 // Create axios instance
 const api = axios.create({
